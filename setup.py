d1b5e19616e0b436108a7d58cfff627ae0800bae
Amit Pandit <i2pandit@gmail.com>
2024-04-01 17:36:24 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT="-e ."
+def get_requirement(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace("\n","")for req in requirement]
+
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+    return requirement        
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Amit",
+    author_email="i2pandit@gmail.com",
+    packeges=find_packages(),
+    install_requires=get_requirement('requirement.txt')
+)
